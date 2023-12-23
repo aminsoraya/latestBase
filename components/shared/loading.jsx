a95bfecb1a286435f6afa4c3d9f0596c401e2de4
basicFlutter <rezvan.mjj@gmail.com>
2023-12-23 09:45:33 +0330

    .

--- components/shared/loading.jsx
+++ components/shared/loading.jsx
@@ -1,6 +1,15 @@
 import styles from "@/styles/loading.module.scss";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 const Loading = () => {
   return <div className={styles.loading}>Loading Data ...</div>;
+};
+
+export const SkeletonLoading = () => {
+  return (
+    <SkeletonTheme baseColor="#202020" highlightColor="#444">
+      <Skeleton />
+    </SkeletonTheme>
+  );
 };
 
 export default Loading;
