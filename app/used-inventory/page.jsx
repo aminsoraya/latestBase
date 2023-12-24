9ef6cf6a350fec281bfa4f612887f77c761da3d9
basicFlutter <rezvan.mjj@gmail.com>
2023-12-24 14:14:37 +0330

    .

--- app/used-inventory/page.jsx
+++ app/used-inventory/page.jsx
@@ -3,21 +3,17 @@ import styles from "@/styles/usedInventory.module.scss";
 import React, { useEffect, useMemo, useState } from "react";
 import { useAppStore } from "@/hooks/store";
 import Container from "@/components/shared/container";
-import DynamicDropdown from "@/components/shared/dynamicDropdown";
-import useSWR, { mutate } from "swr";
-import { useVehicles } from "@/hooks/actions/api/vehicles";
+import { mutate } from "swr";
+import { initialValues } from "@/components/used-inventory/form";
 import {
   SkeletonCardHorizontalLoading,
   SkeletonLoading,
 } from "@/components/shared/loading";
-import { DropDownTypes } from "@/components/shared/dynamicDropdown/enumerations";
-import { useFormik } from "formik";
-import Button from "@/components/shared/button";
-import { IoSearch } from "react-icons/io5";
 import { useContactUs } from "@/hooks/actions/api/contactUs";
 import { PiSquareSplitVerticalFill } from "react-icons/pi";
 import { PiSquareSplitHorizontalFill } from "react-icons/pi";
 import dynamic from "next/dynamic";
+import Form from "@/components/used-inventory/form";
 
 const VerticalCard = dynamic(() =>
   import("@/components/used-inventory/verticalCard")
@@ -27,29 +23,6 @@ const HorzontalCard = dynamic(() =>
   import("@/components/used-inventory/horizontalCard")
 );
 
-const initialValues = {
-  fuel_type: "",
-  body_style: "",
-  engine_cylinders: "",
-  year_end: 2024,
-  price_low: -1,
-  price_high: 3006401,
-  odometer_type: 2,
-  make: "",
-  model: "",
-  transmission: "",
-  drive_train: "",
-  doors: "",
-  interior_color: "",
-  Exterior_color: "",
-  sortKind: { kind: "", type: null, order: 0 },
-  sold: "",
-  is_coming_soon: "",
-  is_it_special: null,
-  year_start: 0,
-  odometer_low: 0,
-  odometer_high: 431000,
-};
 const cardDisplayType = {
   Horizontal: "Horizontal",
   Vertical: "Vertical",
@@ -59,38 +32,13 @@ const UsedInventory = () => {
   const [displayType, setDisplayType] = useState(cardDisplayType.Horizontal);
   const [cars, setCars] = useState([]);
 
-  const formik = useFormik({
-    initialValues: {},
-    onSubmit: async (values, { resetForm }) => {
-      setLoading(true);
-      let response = await mutate(
-        "advanceSearch",
-        useContactUs(
-          initialValues,
-          `${baseUrl}/api/dealership/advance/search/vehicles/${domain}?page=1&limit=10`
-        )
-      );
-      if (response) {
-        setLoading(false);
-        setCars(response);
-      }
-    },
-  });
-
   //base data
-  const { setCurrentMenu, baseUrl, domain, setAdvancedSearchData } =
-    useAppStore();
-
-  //dropdowns data
-  let { data: advancedSearchData, isLoading } = useSWR(
-    `${baseUrl}/api/dealership/advance/search/vehicles/get/${domain}`,
-    useVehicles
-  );
+  const { setCurrentMenu, baseUrl, domain } = useAppStore();
 
   let mounted = true;
   useEffect(() => {
+    setCurrentMenu({ currentMenu: "/used-inventory" });
     mounted &&
-      advancedSearchData &&
       (async () => {
         setLoading(true);
         await mutate(
@@ -107,187 +55,12 @@ const UsedInventory = () => {
     return () => {
       mounted = false;
     };
-  }, [advancedSearchData]);
-
-  //set current menu(active menu)
-  useEffect(() => {
-    setCurrentMenu({ currentMenu: "/used-inventory" });
-  }, []);
-
-  //pass to main store(zustand)
-  useEffect(() => {
-    setAdvancedSearchData({ advancedSearchData });
-  }, [advancedSearchData]);
-
-  cars.length > 0 && console.log("Cars[0]", cars[0]);
+  }, [baseUrl, domain]);
 
   return (
     <div className={styles.main}>
       <Container>
-        <form onSubmit={formik.handleSubmit}>
-          <div className={`row w-100 ${styles.dropDown}`}>
-            <div className="col-lg-3 col-12 mt-3">
-              {isLoading ? (
-                <SkeletonLoading />
-              ) : (
-                <DynamicDropdown
-                  placeholder="Any Make"
-                  type={DropDownTypes.inventory_makes}
-                  callback={(val) => formik.setFieldValue("loanTerm", val)}
-                />
-              )}
-            </div>
-            <div className="col-lg-2 col-12 mt-3">
-              {isLoading ? (
-                <SkeletonLoading />
-              ) : (
-                <DynamicDropdown
-                  placeholder="Min Year"
-                  type={DropDownTypes.inventory_minYear}
-                  callback={(val) => formik.setFieldValue("loanTerm", val)}
-                />
-              )}
-            </div>
-            <div className="col-lg-2 col-12 mt-3">
-              {isLoading ? (
-                <SkeletonLoading />
-              ) : (
-                <DynamicDropdown
-                  placeholder="Max Year"
-                  type={DropDownTypes.inventory_maxYear}
-                  callback={(val) => formik.setFieldValue("loanTerm", val)}
-                />
-              )}
-            </div>
-            <div className="col-lg-3 col-12 mt-3">
-              {isLoading ? (
-                <SkeletonLoading />
-              ) : (
-                <DynamicDropdown
-                  placeholder="Any Model"
-                  type={DropDownTypes.inventory_maxYear}
-                  callback={(val) => formik.setFieldValue("loanTerm", val)}
-                />
-              )}
-            </div>
-            <div className="col-lg-2 col-12 mt-3">
-              {isLoading ? (
-                <SkeletonLoading />
-              ) : (
-                <DynamicDropdown
-                  placeholder="Any Body Style"
-                  type={DropDownTypes.inventory_bodyStyle}
-                  callback={(val) => formik.setFieldValue("loanTerm", val)}
-                />
-              )}
-            </div>
-          </div>
-          <div className={`row w-100 ${styles.dropDown}`}>
-            <div className="col-lg-3 col-12 mt-3">
-              {isLoading ? (
-                <SkeletonLoading />
-              ) : (
-                <DynamicDropdown
-                  placeholder="Any Color"
-                  type={DropDownTypes.inventory_Color}
-                  callback={(val) => formik.setFieldValue("loanTerm", val)}
-                />
-              )}
-            </div>
-            <div className="col-lg-2 col-12 mt-3">
-              {isLoading ? (
-                <SkeletonLoading />
-              ) : (
-                <DynamicDropdown
-                  placeholder="Min Price"
-                  type={DropDownTypes.inventory_MinPrice}
-                  callback={(val) => formik.setFieldValue("loanTerm", val)}
-                />
-              )}
-            </div>
-            <div className="col-lg-2 col-12 mt-3">
-              {isLoading ? (
-                <SkeletonLoading />
-              ) : (
-                <DynamicDropdown
-                  placeholder="Max Price"
-                  type={DropDownTypes.inventory_MaxPrice}
-                  callback={(val) => formik.setFieldValue("loanTerm", val)}
-                />
-              )}
-            </div>
-            <div className="col-lg-3 col-12 mt-3">
-              {isLoading ? (
-                <SkeletonLoading />
-              ) : (
-                <DynamicDropdown
-                  placeholder="Any Transmission"
-                  type={DropDownTypes.transmission}
-                  callback={(val) => formik.setFieldValue("loanTerm", val)}
-                />
-              )}
-            </div>
-            <div className="col-lg-2 col-12 mt-3" style={{ minHeight: "40px" }}>
-              {isLoading ? (
-                <SkeletonLoading />
-              ) : (
-                <Button type="submit">
-                  <IoSearch />
-                  Search
-                </Button>
-              )}
-            </div>
-          </div>
-          <div className={`row w-100 ${styles.dropDown}`}>
-            <div className="col-lg-3 col-12 mt-3">
-              {isLoading ? (
-                <SkeletonLoading />
-              ) : (
-                <DynamicDropdown
-                  placeholder="Any Fuel Type"
-                  type={DropDownTypes.inventory_FuelType}
-                  callback={(val) => formik.setFieldValue("loanTerm", val)}
-                />
-              )}
-            </div>
-            <div className="col-lg-2 col-12 mt-3">
-              {isLoading ? (
-                <SkeletonLoading />
-              ) : (
-                <DynamicDropdown
-                  placeholder="Min Km"
-                  type={DropDownTypes.inventory_MinKm}
-                  callback={(val) => formik.setFieldValue("loanTerm", val)}
-                />
-              )}
-            </div>
-            <div className="col-lg-2 col-12 mt-3">
-              {isLoading ? (
-                <SkeletonLoading />
-              ) : (
-                <DynamicDropdown
-                  placeholder="Max Km"
-                  type={DropDownTypes.inventory_MaxKm}
-                  callback={(val) => formik.setFieldValue("loanTerm", val)}
-                />
-              )}
-            </div>
-            <div className="col-lg-3 col-12 mt-3">
-              {isLoading ? (
-                <SkeletonLoading />
-              ) : (
-                <DynamicDropdown
-                  placeholder="Any Engine"
-                  type={DropDownTypes.inventory_Engine}
-                  callback={(val) => formik.setFieldValue("loanTerm", val)}
-                />
-              )}
-            </div>
-            <div className="col-lg-2 col-12 mt-3" style={{ minHeight: "40px" }}>
-              {isLoading ? <SkeletonLoading /> : <Button>Reset</Button>}
-            </div>
-          </div>
-        </form>
+        <Form />
         {!loading && (
           <div className="row">
             <div className={`col-12 mt-5 mb-2 ${styles.headerResponse}`}>
@@ -337,7 +110,6 @@ const UsedInventory = () => {
                 })}
               </>
             )}
-
             {cars.length > 0 && (
               <>
                 {cars?.map((car, index) => {
---
+++ components/used-inventory/form.jsx
@@ -0,0 +1,240 @@
+import Button from "@/components/shared/button";
+import { IoSearch } from "react-icons/io5";
+import DynamicDropdown from "@/components/shared/dynamicDropdown";
+import { DropDownTypes } from "@/components/shared/dynamicDropdown/enumerations";
+import { useFormik } from "formik";
+import styles from "@/styles/usedInventory.module.scss";
+import { useState } from "react";
+import { SkeletonLoading } from "@/components/shared/loading";
+import { useAppStore } from "@/hooks/store";
+import useSWR from "swr";
+import { useVehicles } from "@/hooks/actions/api/vehicles";
+import { useEffect } from "react";
+
+export const initialValues = {
+  fuel_type: "",
+  body_style: "",
+  engine_cylinders: "",
+  year_end: 2024,
+  price_low: -1,
+  price_high: 3006401,
+  odometer_type: 2,
+  make: "",
+  model: "",
+  transmission: "",
+  drive_train: "",
+  doors: "",
+  interior_color: "",
+  Exterior_color: "",
+  sortKind: { kind: "", type: null, order: 0 },
+  sold: "",
+  is_coming_soon: "",
+  is_it_special: null,
+  year_start: 0,
+  odometer_low: 0,
+  odometer_high: 431000,
+};
+const Form = () => {
+  const [loading, setLoading] = useState(true);
+
+  //base data
+  const { setCurrentMenu, baseUrl, domain, setAdvancedSearchData } =
+    useAppStore();
+
+  //dropdowns data
+  let { data: advancedSearchData, isLoading } = useSWR(
+    `${baseUrl}/api/dealership/advance/search/vehicles/get/${domain}`,
+    useVehicles
+  );
+
+  //pass to main store(zustand)
+  useEffect(() => {
+    setAdvancedSearchData({ advancedSearchData });
+  }, [advancedSearchData]);
+
+  const formik = useFormik({
+    initialValues: {},
+    onSubmit: async (values, { resetForm }) => {
+      setLoading(true);
+      let response = await mutate(
+        "advanceSearch",
+        useContactUs(
+          initialValues,
+          `${baseUrl}/api/dealership/advance/search/vehicles/${domain}?page=1&limit=10`
+        )
+      );
+      if (response) {
+        setLoading(false);
+        setCars(response);
+      }
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div className={`row w-100 ${styles.dropDown}`}>
+        <div className="col-lg-3 col-12 mt-3">
+          {isLoading ? (
+            <SkeletonLoading />
+          ) : (
+            <DynamicDropdown
+              placeholder="Any Make"
+              type={DropDownTypes.inventory_makes}
+              callback={(val) => formik.setFieldValue("loanTerm", val)}
+            />
+          )}
+        </div>
+        <div className="col-lg-2 col-12 mt-3">
+          {isLoading ? (
+            <SkeletonLoading />
+          ) : (
+            <DynamicDropdown
+              placeholder="Min Year"
+              type={DropDownTypes.inventory_minYear}
+              callback={(val) => formik.setFieldValue("loanTerm", val)}
+            />
+          )}
+        </div>
+        <div className="col-lg-2 col-12 mt-3">
+          {isLoading ? (
+            <SkeletonLoading />
+          ) : (
+            <DynamicDropdown
+              placeholder="Max Year"
+              type={DropDownTypes.inventory_maxYear}
+              callback={(val) => formik.setFieldValue("loanTerm", val)}
+            />
+          )}
+        </div>
+        <div className="col-lg-3 col-12 mt-3">
+          {isLoading ? (
+            <SkeletonLoading />
+          ) : (
+            <DynamicDropdown
+              placeholder="Any Model"
+              type={DropDownTypes.inventory_maxYear}
+              callback={(val) => formik.setFieldValue("loanTerm", val)}
+            />
+          )}
+        </div>
+        <div className="col-lg-2 col-12 mt-3">
+          {isLoading ? (
+            <SkeletonLoading />
+          ) : (
+            <DynamicDropdown
+              placeholder="Any Body Style"
+              type={DropDownTypes.inventory_bodyStyle}
+              callback={(val) => formik.setFieldValue("loanTerm", val)}
+            />
+          )}
+        </div>
+      </div>
+      <div className={`row w-100 ${styles.dropDown}`}>
+        <div className="col-lg-3 col-12 mt-3">
+          {isLoading ? (
+            <SkeletonLoading />
+          ) : (
+            <DynamicDropdown
+              placeholder="Any Color"
+              type={DropDownTypes.inventory_Color}
+              callback={(val) => formik.setFieldValue("loanTerm", val)}
+            />
+          )}
+        </div>
+        <div className="col-lg-2 col-12 mt-3">
+          {isLoading ? (
+            <SkeletonLoading />
+          ) : (
+            <DynamicDropdown
+              placeholder="Min Price"
+              type={DropDownTypes.inventory_MinPrice}
+              callback={(val) => formik.setFieldValue("loanTerm", val)}
+            />
+          )}
+        </div>
+        <div className="col-lg-2 col-12 mt-3">
+          {isLoading ? (
+            <SkeletonLoading />
+          ) : (
+            <DynamicDropdown
+              placeholder="Max Price"
+              type={DropDownTypes.inventory_MaxPrice}
+              callback={(val) => formik.setFieldValue("loanTerm", val)}
+            />
+          )}
+        </div>
+        <div className="col-lg-3 col-12 mt-3">
+          {isLoading ? (
+            <SkeletonLoading />
+          ) : (
+            <DynamicDropdown
+              placeholder="Any Transmission"
+              type={DropDownTypes.transmission}
+              callback={(val) => formik.setFieldValue("loanTerm", val)}
+            />
+          )}
+        </div>
+        <div className="col-lg-2 col-12 mt-3" style={{ minHeight: "40px" }}>
+          {isLoading ? (
+            <SkeletonLoading />
+          ) : (
+            <Button type="submit">
+              <IoSearch />
+              Search
+            </Button>
+          )}
+        </div>
+      </div>
+      <div className={`row w-100 ${styles.dropDown}`}>
+        <div className="col-lg-3 col-12 mt-3">
+          {isLoading ? (
+            <SkeletonLoading />
+          ) : (
+            <DynamicDropdown
+              placeholder="Any Fuel Type"
+              type={DropDownTypes.inventory_FuelType}
+              callback={(val) => formik.setFieldValue("loanTerm", val)}
+            />
+          )}
+        </div>
+        <div className="col-lg-2 col-12 mt-3">
+          {isLoading ? (
+            <SkeletonLoading />
+          ) : (
+            <DynamicDropdown
+              placeholder="Min Km"
+              type={DropDownTypes.inventory_MinKm}
+              callback={(val) => formik.setFieldValue("loanTerm", val)}
+            />
+          )}
+        </div>
+        <div className="col-lg-2 col-12 mt-3">
+          {isLoading ? (
+            <SkeletonLoading />
+          ) : (
+            <DynamicDropdown
+              placeholder="Max Km"
+              type={DropDownTypes.inventory_MaxKm}
+              callback={(val) => formik.setFieldValue("loanTerm", val)}
+            />
+          )}
+        </div>
+        <div className="col-lg-3 col-12 mt-3">
+          {isLoading ? (
+            <SkeletonLoading />
+          ) : (
+            <DynamicDropdown
+              placeholder="Any Engine"
+              type={DropDownTypes.inventory_Engine}
+              callback={(val) => formik.setFieldValue("loanTerm", val)}
+            />
+          )}
+        </div>
+        <div className="col-lg-2 col-12 mt-3" style={{ minHeight: "40px" }}>
+          {isLoading ? <SkeletonLoading /> : <Button>Reset</Button>}
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
