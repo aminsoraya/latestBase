7e8c209565bac67935541e1713dd4c4ee5ad2245
aminsoraya <aminsoraya@gmail.com>
2023-12-28 17:30:45 +0330

    .

--- components/shared/dynamicDropdown/inventory/maxYear.jsx
+++ components/shared/dynamicDropdown/inventory/maxYear.jsx
@@ -1,14 +1,23 @@
 import { useAppStore } from "@/hooks/store";
 import styles from "@/styles/inventoryItems.module.scss";
+import { useMemo } from "react";
 
-const Models = (props) => {
+const MaxYear = (props) => {
   const {
+    allObjects: { year },
     advancedSearchData: { vehicleYear_full },
   } = useAppStore();
 
-  const makes = vehicleYear_full ? Object.keys(vehicleYear_full) : [];
+  const years = useMemo(() => {
+    if (year) {
+      return year;
+    } else if (vehicleYear_full) {
+      return Object.keys(vehicleYear_full);
+    }
+    return [];
+  }, [year, vehicleYear_full]);
 
-  return makes
+  return years
     .sort((a, b) => b - a)
     .map((value, index) => (
       <span
@@ -21,4 +30,4 @@ const Models = (props) => {
     ));
 };
 
-export default Models;
+export default MaxYear;
--- components/shared/dynamicDropdown/inventory/minYear.jsx
+++ components/shared/dynamicDropdown/inventory/minYear.jsx
@@ -1,14 +1,23 @@
 import { useAppStore } from "@/hooks/store";
 import styles from "@/styles/inventoryItems.module.scss";
+import { useMemo } from "react";
 
-const Models = (props) => {
+const MinYear = (props) => {
   const {
+    allObjects: { year },
     advancedSearchData: { vehicleYear_full },
   } = useAppStore();
 
-  const makes = vehicleYear_full ? Object.keys(vehicleYear_full) : [];
+  const years = useMemo(() => {
+    if (year) {
+      return year;
+    } else if (vehicleYear_full) {
+      return Object.keys(vehicleYear_full);
+    }
+    return [];
+  }, [year, vehicleYear_full]);
 
-  return makes
+  return years
     .sort((a, b) => a - b)
     .map((value, index) => (
       <span
@@ -21,4 +30,4 @@ const Models = (props) => {
     ));
 };
 
-export default Models;
+export default MinYear;
