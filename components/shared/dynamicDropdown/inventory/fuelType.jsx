8120b47da8e1c86c95531bc2d0aeecd701c8f40d
aminsoraya <aminsoraya@gmail.com>
2023-12-28 21:49:01 +0330

    .

--- components/shared/dynamicDropdown/inventory/fuelType.jsx
+++ components/shared/dynamicDropdown/inventory/fuelType.jsx
@@ -1,22 +1,33 @@
 import { useAppStore } from "@/hooks/store";
 import styles from "@/styles/inventoryItems.module.scss";
+import { useMemo } from "react";
 
 const FuelType = (props) => {
   const {
+    allObjects: { Fuel_type },
     advancedSearchData: { vehicleFuel_type_full },
   } = useAppStore();
 
-  const typeFuel = Object.keys(vehicleFuel_type_full);
+  const fuelType = useMemo(() => {
+    if (Fuel_type) {
+      return Object.values(Fuel_type);
+    } else if (vehicleFuel_type_full) {
+      return Object.values(vehicleFuel_type_full);
+    }
+    return [];
+  }, [Fuel_type, vehicleFuel_type_full]);
 
-  return typeFuel?.map((value, index) => (
-    <span
-      key={index}
-      className={styles.item}
-      onClick={() => props.callback({ key: value, alias: value })}
-    >
-      {value}
-    </span>
-  ));
+  return fuelType
+    .sort((a, b) => a - b)
+    .map((value, index) => (
+      <span
+        key={index}
+        className={styles.item}
+        onClick={() => props.callback({ key: value, alias: value })}
+      >
+        {value}
+      </span>
+    ));
 };
 
 export default FuelType;
