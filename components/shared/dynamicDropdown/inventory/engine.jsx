129971da76a1b434162fea9e0b01665c354828d1
aminsoraya <aminsoraya@gmail.com>
2023-12-29 22:07:18 +0330

    .

--- components/shared/dynamicDropdown/inventory/engine.jsx
+++ components/shared/dynamicDropdown/inventory/engine.jsx
@@ -1,6 +1,7 @@
 import { useAppStore } from "@/hooks/store";
 import styles from "@/styles/inventoryItems.module.scss";
 import { useMemo } from "react";
+import { GetSpecificField } from "@/hooks/actions/useInventoryUrl";
 
 const EngineCylinder = (props) => {
   const {
@@ -8,9 +9,13 @@ const EngineCylinder = (props) => {
     advancedSearchData: { vehicleEngine_cylinders_full },
   } = useAppStore();
 
+  let urlEngineCylinder = GetSpecificField("EngineCylinder");
+
   const engine = useMemo(() => {
-    if (engine_cylinders) {
-      return  engine_cylinders ;
+    if (urlEngineCylinder) {
+      return vehicleEngine_cylinders_full[urlEngineCylinder].engine_cylinders;
+    } else if (engine_cylinders) {
+      return engine_cylinders;
     } else if (vehicleEngine_cylinders_full) {
       return Object.keys(vehicleEngine_cylinders_full);
     }
