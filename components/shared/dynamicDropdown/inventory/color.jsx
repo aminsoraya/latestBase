d0f052927f30fd5013d374305cfe3df3e3c88fea
basicFlutter <rezvan.mjj@gmail.com>
2023-12-30 11:57:20 +0330

    .

--- components/shared/dynamicDropdown/inventory/color.jsx
+++ components/shared/dynamicDropdown/inventory/color.jsx
@@ -1,20 +1,52 @@
 import { useAppStore } from "@/hooks/store";
 import styles from "@/styles/inventoryItems.module.scss";
+import { useMemo } from "react";
+import { GetSpecificField, useField } from "@/hooks/actions/useInventoryUrl";
 
-const Colors = (props) => {
+const Color = (props) => {
   const {
-    advancedSearchData: { color },
+    allObjects: { exterior_color },
+    allObjects,
+    advancedSearchData: { vehicleexterior_color_full },
   } = useAppStore();
 
-  return color.map((value, index) => (
-    <span
-      key={index}
-      className={styles.item}
-      onClick={() => props.callback({ key: value.name, alias: value.name })}
-    >
-      {value.name}
-    </span>
-  ));
+  console.log("allObjects ", allObjects);
+  let urlExteriorColor = GetSpecificField("Exteriorcolor");
+
+  const { SetBaseField } = useField();
+
+  let baseField = GetSpecificField("Base");
+  const style = useMemo(() => {
+    //when determined as a base filed
+    if (baseField && baseField != "" && baseField == "Exteriorcolor") {
+      return Object.keys(vehicleexterior_color_full);
+    } else if (urlExteriorColor) {
+      //when determined in url
+      return vehicleexterior_color_full[urlExteriorColor].exterior_color;
+    } else if (exterior_color) {
+      //when present in all object
+      return exterior_color;
+    } else if (vehicleexterior_color_full) {
+      //any mode
+      return Object.keys(vehicleexterior_color_full);
+    }
+    return [];
+  }, [exterior_color, vehicleexterior_color_full]);
+
+  return style
+    .sort((a, b) => a - b)
+    .map((value, index) => (
+      <span
+        key={index}
+        className={styles.item}
+        onClick={() => {
+          SetBaseField("Base", "Exteriorcolor");
+          props.callback({ key: value, alias: value });
+        }}
+      >
+        {value}
+      </span>
+    ));
 };
 
-export default Colors;
+export default Color;
